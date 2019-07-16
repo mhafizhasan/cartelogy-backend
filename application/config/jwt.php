f7129deaaf70f804b5fdc994bf3e810ebc3e6425
Muhammad Hafiz Hasan <mhafizhasan@gmail.com>
2019-07-16 12:50:41 +0800

    add required libraries and helpers

---
+++ application/config/jwt.php
@@ -0,0 +1,4 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+// password_hash('c4r73l09y', PASSWORD_DEFAULT)
+$config['jwt_key'] = '$2y$10$M5C0XE.htwDzucs3pdH4X.7YpggLvLc7HcYEJliTKh9u6WSnPzbFe';
